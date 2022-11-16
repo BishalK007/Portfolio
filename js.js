e0357ccc2fd2bd66dd4a34a02333fc481385da25
bishal <bishalkarmakar007@gmail.com>
2022-11-16 23:19:50 +0530

    commit_3_16-11

--- js.js
+++ js.js
@@ -9,6 +9,24 @@ hamMenu.addEventListener('click', () => {
     element.classList.toggle('active');
 
 });
+
+function contactButtonPressed() {
+    const contactPage = document.getElementById("Contact_Page");
+    if(contactPage.classList.contains('hidden')){
+        contactPage.classList.remove('hidden');
+        contactPage.classList.toggle('active');
+    } else {
+        contactPage.classList.toggle('active');
+        contactPage.classList.add('hidden');
+    }
+}
+const contactButton = document.getElementsByClassName('contact_button');
+const contactButton0 = contactButton[0];
+const contactButton1 = contactButton[1];
+contactButton0.addEventListener('click', contactButtonPressed);
+contactButton1.addEventListener('click', contactButtonPressed);
+
+
 // const scrollBox = document.getElementById('Scroll_Box');
 // scrollBox.style.height = screen.height - document.getElementById('Desktop_Navbar').height - document.getElementById('Mobile_Navbar').height; 
 // scrollBox.style.width = screen.width - document.getElementById('Desktop_Navbar').width - document.getElementById('Mobile_Navbar').width; 
